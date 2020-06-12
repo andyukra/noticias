6094e92364ac8536ee5ed1cbe8a300dd4c0e5da5
andy <andyguachin87@gmail.com>
2020-06-12 20:26:44 -0300

    v1

---
+++ precache-manifest.00aeb21a6ce072c0e9a97d3d7ccad300.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8898228ba2cda602c802bd0401b5b0",
+    "url": "/noticias/index.html"
+  },
+  {
+    "revision": "47368d1d727027b30196",
+    "url": "/noticias/static/js/2.a8ca0ba7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/noticias/static/js/2.a8ca0ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8a313993232d072d31",
+    "url": "/noticias/static/js/main.09f85b6d.chunk.js"
+  },
+  {
+    "revision": "195de5e120152eb6cbe2",
+    "url": "/noticias/static/js/runtime-main.5beb0841.js"
+  }
+]);
